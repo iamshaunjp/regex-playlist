5b40032d60a7bbfe673732d47e70ec1552dde258
iamshaunjp <shaunjp@shaun-pelling.com>
2018-02-12 11:37:07 +0000

    lesson 13 code

--- validation.js
+++ validation.js
@@ -1,4 +1,25 @@
+const inputs = document.querySelectorAll('input');
+
 // regex patterns
 const patterns = {
         telephone: /^\d{11}$/
 };
+
+// validation function
+function validate(field, regex){
+
+    if(regex.test(field.value)){
+        field.className = 'valid';
+    } else {
+        field.className = 'invalid';
+    }
+
+}
+
+// attach keyup events to inputs
+inputs.forEach((input) => {
+    input.addEventListener('keyup', (e) => {
+            // console.log(patterns[e.target.attributes.name.value]);
+            validate(e.target, patterns[e.target.attributes.name.value]);
+    });
+});
