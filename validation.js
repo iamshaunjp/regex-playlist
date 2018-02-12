208a834292b12ffc8fdfb6375bb53f479338a654
iamshaunjp <shaunjp@shaun-pelling.com>
2018-02-12 10:50:44 +0000

    absolute final files

--- validation.js
+++ validation.js
@@ -5,8 +5,9 @@ const patterns = {
         telephone: /^\d{11}$/,
         slug: /^[a-z0-9-]+$/,
         username: /^[a-z]{5,12}$/i,
-        email: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,12})$/,
-        password: /^[a-z0-9_-]{8,20}$/
+        password: /^[a-z0-9_-]{8,20}$/,
+        email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
+        //             yourname @ domain   .  com          ( .uk )
 };
 
 // validation function
